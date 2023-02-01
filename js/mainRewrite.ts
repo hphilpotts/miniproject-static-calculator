7f1ad90267049ac1944d774310e1a7a8e1b2cf47
Henry Philpotts <hphilpotts@gmail.com>
2023-02-01 11:01:23 +0000

    mainRewrite converted to TypeScript and fully typed, successfully compiled.

--- js/mainRewrite.ts
+++ js/mainRewrite.ts
@@ -8,7 +8,7 @@ let prevRightNum: GlobalVariable = null; // the previously input currentRightNum
 let currentOperator: GlobalVariable = null; // the most recently pressed operator, held ready for eval in the event of `=` or binary operator press
 let prevOperator: GlobalVariable = null; // the previously used binary operator, allows for repeated press of `=` 
 
-const resetAll = () => { // Full reset of all Global variables to null
+const resetAll = ():void => { // Full reset of all Global variables to null
     leftNum = null; currentRightNum = null; prevRightNum = null; currentOperator = null; prevOperator = null;
 };
 
@@ -18,7 +18,7 @@ const resetAll = () => { // Full reset of all Global variables to null
 const buttons = document.querySelectorAll(".button-inner");
 
 // Get button innerHTML string, pass this as input to processButtonPress:
-function handleClick(){
+function handleClick():void {
     let buttonPress: string = this.innerHTML;
     processButtonPress(buttonPress); // see section immediately below:
 };
@@ -29,12 +29,12 @@ buttons.forEach(button => button.addEventListener('click', handleClick));
 // * -- Processing button presses:
 
 // input is the innerHTML from the button being pressed, passed from handleClick() above:
-const processButtonPress = (input: string) => {
+const processButtonPress = (input: string): void => {
 
     // button innerHTML strings grouped by type (with respective functions to run for each type)
-    const unaryOperators = ['+/-', '%', '√'];
-    const binaryOperators = ['/', 'x', '-', '+'];
-    const numbers = new RegExp("[0-9]") // regex previously included '.' as well, however this caused every other `numbers.test(input)` to fail
+    const unaryOperators: string[] = ['+/-', '%', '√'];
+    const binaryOperators: string[] = ['/', 'x', '-', '+'];
+    const numbers: RegExp = new RegExp("[0-9]") // regex previously included '.' as well, however this caused every other `numbers.test(input)` to fail
     switch (true) {
         case (numbers.test(input) || input === '.'): // decimal can be mostly handled the same way as a number (press): i.e. as string
             numberPress(input); break
@@ -52,7 +52,7 @@ const processButtonPress = (input: string) => {
 };
 
 // when number button is pressed, take the number pressed (as string), join on to to currentRightNum string and set display accordingly: 
-const numberPress = (numberPressed: string) => {
+const numberPress = (numberPressed: string): void => {
     if (numberPressed === '.') { // decimals need to be handled a little differently
         if (!currentRightNum) currentRightNum = leftNum; // allows for most recent result to be updated with decimal point
         if (currentRightNum.includes('.')) numberPressed = ''; // prevents more than one decimal point being added
@@ -63,7 +63,7 @@ const numberPress = (numberPressed: string) => {
 };
 
 // when a unary operator is pressed, determine from string passed in what operation to perform, then evaluate, then update globals: 
-const unaryOperatorPress = (unaryPressed: string) =>  {
+const unaryOperatorPress = (unaryPressed: string): void =>  {
     if (unaryPressed === '+/-') { // despite also being a unary operator, it turns out '+/-' needs to be handled differently
         switch (true) {
             case (currentRightNum != null): // if there is currentRightNum then operate on this
@@ -80,7 +80,7 @@ const unaryOperatorPress = (unaryPressed: string) =>  {
                 setDisplay(currentRightNum);
         }
     } else { // other unary operators are handled simply:
-        let operand = null;
+        let operand: number | null = null;
         (!currentRightNum) ? operand = +leftNum : operand = +currentRightNum;
         switch (unaryPressed) {
             case '%': operand = operand / 100; break
@@ -93,8 +93,8 @@ const unaryOperatorPress = (unaryPressed: string) =>  {
 };
 
 // when a binary operator is pressed, determine from string passed in what operation to perform, then evaluate, then update globals: 
-const binaryOperatorPress = (binaryPressed: string) => {
-    let operand1 = +leftNum, operand2 = +currentRightNum;
+const binaryOperatorPress = (binaryPressed: string): void => {
+    let operand1: number = +leftNum, operand2: number = +currentRightNum;
     if (leftNum && currentOperator && currentRightNum) { // if sum already present in saved vars, evaluate and save binaryPressed as new currentOperator
         leftNum = performBinaryOperation(operand1, binaryPressed, operand2); // see function below equalsPress()
         currentOperator = binaryPressed;  
@@ -111,8 +111,8 @@ const binaryOperatorPress = (binaryPressed: string) => {
 }
 
 // TODO - either incorporate into binaryOperatorPress or break down into functions accessible to both equalsPress and binaryOperatorPress
-const equalsPress = () => {
-    let operand1 = +leftNum, operand2 = null;
+const equalsPress = (): void => {
+    let operand1: number = +leftNum, operand2: number = null;
     if (leftNum && currentOperator && currentRightNum) {
         operand2 = +currentRightNum;
         leftNum = performBinaryOperation(operand1, currentOperator, operand2);
@@ -131,8 +131,8 @@ const equalsPress = () => {
 }
 
 // Used in both binaryOperatorPress and equalsPress:
-const performBinaryOperation = (val1: number, operator: string, val2: number) => {
-    let output = null;
+const performBinaryOperation = (val1: number, operator: string, val2: number): string => {
+    let output: number | null = null;
     switch (operator) {
         case ('+'): output = val1 + val2; break
         case ('-'): output = val1 - val2; break
@@ -143,18 +143,18 @@ const performBinaryOperation = (val1: number, operator: string, val2: number) =>
     return (output*1).toString(); // the *1 eliminates any unneccessary trailing 0s
 };
 
-const cancel = () => {
+const cancel = (): void => {
     resetAll();
     clearDisplay();
 };
 
 // * -- Update Display:
 
-const display = document.getElementById('screen-display');
+const display: HTMLElement = document.getElementById('screen-display');
 
 // takes in a string (representing a number) and renders this in the calculator's display
-const setDisplay = (numberAsString: string) => {
-    let displayValue = numberAsString
+const setDisplay = (numberAsString: string): void => {
+    let displayValue: string = numberAsString
     // handle floats in order to fit number displayed to screen, if very large float (>99999999) display error, else trim down
     if (numberAsString.includes('.')) (numberAsString.indexOf('.') > 9) ? displayValue ="err" : displayValue = numberAsString.slice(0, 8);
     // handle large ints / very small floats
@@ -163,18 +163,18 @@ const setDisplay = (numberAsString: string) => {
     display.innerText = displayValue;
 }
 
-const clearDisplay = () => display.innerText = '';
+const clearDisplay = ():string => display.innerText = '';
 
 // * -- Change Modes:
 
-const modeChangeButtons = document.getElementsByClassName('control-button');
-const styleSheet = document.getElementById('current-stylesheet');
-let currentStylesheet = styleSheet.getAttribute('href');
+const modeChangeButtons: HTMLCollection = document.getElementsByClassName('control-button');
+const styleSheet: HTMLElement = document.getElementById('current-stylesheet');
+let currentStylesheet: string = styleSheet.getAttribute('href');
 
 // light mode:
-const setLightMode = () => styleSheet.setAttribute('href', 'css/lightmode.css')
+const setLightMode = (): void => styleSheet.setAttribute('href', 'css/lightmode.css')
 modeChangeButtons[0].addEventListener('click', setLightMode);
 
 // dark mode:
-const setDarkMode = () => styleSheet.setAttribute('href', 'css/nightmode.css')
+const setDarkMode = (): void => styleSheet.setAttribute('href', 'css/nightmode.css')
 modeChangeButtons[1].addEventListener('click', setDarkMode)
